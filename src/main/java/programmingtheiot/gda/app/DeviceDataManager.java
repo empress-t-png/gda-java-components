80b74969f2ebced41e7405e16d3df44056ba8971
Tosin <tosin.e@northeastern.edu>
2025-10-27 20:37:33 -0400

    Completed Lab Module 07-004: Integrated MqttClientConnector with DeviceDataManager

--- src/main/java/programmingtheiot/gda/app/DeviceDataManager.java
+++ src/main/java/programmingtheiot/gda/app/DeviceDataManager.java
@@ -36,7 +36,7 @@ import programmingtheiot.gda.connection.SmtpClientConnector;
 import programmingtheiot.gda.system.SystemPerformanceManager;
 
 /**
- * Implementation of DeviceDataManager for Lab Module 05.
+ * Implementation of DeviceDataManager for Lab Module 07.
  *
  */
 public class DeviceDataManager implements IDataMessageListener
@@ -56,7 +56,7 @@ public class DeviceDataManager implements IDataMessageListener
 	private boolean enableSystemPerf = false;
 	
 	private IActuatorDataListener actuatorDataListener = null;
-	private IPubSubClient mqttClient = null;
+	private MqttClientConnector mqttClient = null;
 	private IPubSubClient cloudClient = null;
 	private IPersistenceClient persistenceClient = null;
 	private IRequestResponseClient smtpClient = null;
@@ -190,6 +190,31 @@ public class DeviceDataManager implements IDataMessageListener
 	{
 		_Logger.info("Starting DeviceDataManager...");
 		
+		if (this.mqttClient != null) {
+			if (this.mqttClient.connectClient()) {
+				_Logger.info("Successfully connected MQTT client to broker.");
+				
+				// add necessary subscriptions
+				
+				// TODO: read this from the configuration file
+				int qos = ConfigConst.DEFAULT_QOS;
+				
+				// TODO: check the return value for each and take appropriate action
+				
+				// IMPORTANT NOTE: The 'subscribeToTopic()' method calls shown
+				// below will be moved to MqttClientConnector.connectComplete()
+				// in Lab Module 10. For now, they can remain here.
+				this.mqttClient.subscribeToTopic(ResourceNameEnum.GDA_MGMT_STATUS_MSG_RESOURCE, qos);
+				this.mqttClient.subscribeToTopic(ResourceNameEnum.CDA_ACTUATOR_RESPONSE_RESOURCE, qos);
+				this.mqttClient.subscribeToTopic(ResourceNameEnum.CDA_SENSOR_MSG_RESOURCE, qos);
+				this.mqttClient.subscribeToTopic(ResourceNameEnum.CDA_SYSTEM_PERF_MSG_RESOURCE, qos);
+			} else {
+				_Logger.severe("Failed to connect MQTT client to broker.");
+				
+				// TODO: take appropriate action
+			}
+		}
+		
 		if (this.sysPerfMgr != null) {
 			this.sysPerfMgr.startManager();
 		}
@@ -201,6 +226,30 @@ public class DeviceDataManager implements IDataMessageListener
 		
 		if (this.sysPerfMgr != null) {
 			this.sysPerfMgr.stopManager();
+		}
+		
+		if (this.mqttClient != null) {
+			// add necessary un-subscribes
+			
+			// TODO: check the return value for each and take appropriate action
+			
+			// NOTE: The unsubscribeFromTopic() method calls below should match with
+			// the subscribeToTopic() method calls from startManager(). Also, the
+			// unsubscribe logic below can be moved to MqttClientConnector's
+			// disconnectClient() call PRIOR to actually disconnecting from
+			// the MQTT broker.
+			this.mqttClient.unsubscribeFromTopic(ResourceNameEnum.GDA_MGMT_STATUS_MSG_RESOURCE);
+			this.mqttClient.unsubscribeFromTopic(ResourceNameEnum.CDA_ACTUATOR_RESPONSE_RESOURCE);
+			this.mqttClient.unsubscribeFromTopic(ResourceNameEnum.CDA_SENSOR_MSG_RESOURCE);
+			this.mqttClient.unsubscribeFromTopic(ResourceNameEnum.CDA_SYSTEM_PERF_MSG_RESOURCE);
+			
+			if (this.mqttClient.disconnectClient()) {
+				_Logger.info("Successfully disconnected MQTT client from broker.");
+			} else {
+				_Logger.severe("Failed to disconnect MQTT client from broker.");
+				
+				// TODO: take appropriate action
+			}
 		}
 	}
 	
@@ -219,26 +268,4 @@ public class DeviceDataManager implements IDataMessageListener
 			configUtil.getBoolean(ConfigConst.GATEWAY_DEVICE, ConfigConst.ENABLE_SYSTEM_PERF_KEY);
 		
 		if (this.enableSystemPerf) {
-			this.sysPerfMgr = new SystemPerformanceManager();
-			this.sysPerfMgr.setDataMessageListener(this);
-		}
-		
-		// Connection classes will be implemented in later lab modules
-		if (this.enableMqttClient) {
-			// TODO: implement this in Lab Module 7
-		}
-		
-		if (this.enableCoapServer) {
-			// TODO: implement this in Lab Module 8
-		}
-		
-		if (this.enableCloudClient) {
-			// TODO: implement this in Lab Module 10
-		}
-		
-		if (this.enablePersistenceClient) {
-			// TODO: implement this as an optional exercise in Lab Module 5
-		}
-	}
-	
-}
+			this.sysPerfMgr = new SystemPerformanceMana
